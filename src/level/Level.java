136b7521d6d0fb0a31283447a9b25b9c768cf335
ali <rheagar.grk@gmail.com>
2021-06-16 19:39:34 +0430

    project so far, 6/16/2021 7:39 pm cont Level class imp

--- src/level/Level.java
+++ src/level/Level.java
@@ -1,4 +1,22 @@
 package level;
 
+import model.animals.AnimalTypes;
+
+import java.util.HashMap;
+
 public class Level {
+    // primary level properties
+    int initialCoins;
+    Task task;
+    HashMap <Integer, AnimalTypes> spawn;
+    int maxTime;
+    int reward;
+
+    public Level(int initialCoins, Task task, HashMap<Integer, AnimalTypes> spawn, int maxTime, int reward) {
+        this.initialCoins = initialCoins;
+        this.task = task;
+        this.spawn = spawn;
+        this.maxTime = maxTime;
+        this.reward = reward;
+    }
 }
