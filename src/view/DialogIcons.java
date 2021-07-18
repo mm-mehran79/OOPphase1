1f5837cc89d6932cc49e08c585bf40fca413b6c1
Mehran <mm.mehran79@gmail.com>
2021-07-18 17:44:31 +0430

    GraphicalLoginMenu ended

---
+++ src/view/DialogIcons.java
@@ -0,0 +1,8 @@
+package view;
+
+import javax.swing.*;
+
+public class DialogIcons {
+    public static Icon ERROR_ICON = new ImageIcon("./data/error.png");
+    public static Icon INFORMATION = new ImageIcon("./data/information.png");
+}
